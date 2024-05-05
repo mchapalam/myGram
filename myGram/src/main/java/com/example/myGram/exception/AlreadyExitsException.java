82d0bd4444efb3568db504ec1659e909aa9b9656
mchapalam <46065355+mchapalam@users.noreply.github.com>
2024-05-05 13:21:32 +0600

    Delete myGram directory

--- myGram/src/main/java/com/example/myGram/exception/AlreadyExitsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.myGram.exception;
-
-public class AlreadyExitsException extends RuntimeException{
-    public AlreadyExitsException(String message) {
-        super(message);
-    }
-}
